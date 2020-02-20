0a715f2c2ad62a8423c560bd2d4cae740d1d2aa1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-02-20 12:14:17 -0500

    Add the template version of the WordHide with the TODO markers.

---
+++ src/main/java/labfive/WordHide.java
@@ -0,0 +1,34 @@
+package labfive;
+
+// TODO: Add the necessary import statements
+// TODO: Make sure that you only add those imports that are needed!
+
+// TODO: Add the correct JavaDoc comment for this class
+
+public class WordHide {
+
+  // TODO: Make sure that you understand the meaning of the "public" visibility modifier
+  //
+// TODO: Add the correct JavaDoc comment for this method
+
+  public static void main(String[] args) {
+    // TODO: display the name of the programmer and the date
+
+    // TODO: declare the starting file and scanner
+
+    // TODO: connect the scanner to the input file
+
+    // TODO: Read in and display the chosen word
+
+    // TODO: Repeatedly use substring and charAt to extract parts of the word
+
+    // TODO: Create rows and/or columns for the 20x20 grid of letters
+
+    // TODO: Hide the word by declaring and creating new String variables
+
+    // TODO: Display each row and/or column of the 20x20 grid of letters
+
+    // TODO: Display and thankyou message for the program
+  }
+
+}
